184101a5e6e3ab3f862ddcd566eb2370be075bae
Carlos Buruel <charly.ob.26@gmail.com>
2025-04-11 14:43:59 -0600

    [CBO] Parallax image on detail

--- lib/presentation/views/meal_detail_view.dart
+++ lib/presentation/views/meal_detail_view.dart
@@ -3,7 +3,25 @@ import 'package:yumly_flutter_app/domain/entities/entities.dart';
 import 'package:yumly_flutter_app/shared/widgets/widgets.dart';
 
 class MealDetailView extends StatelessWidget {
-  const MealDetailView({
+  const MealDetailView({super.key, required this.mealEntity});
+
+  final MealEntity mealEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          CustomSilverAppBar(mealEntity: mealEntity),
+          CustomSliverToBoxAdapter(mealEntity: mealEntity),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSliverToBoxAdapter extends StatelessWidget {
+  const CustomSliverToBoxAdapter({
     super.key,
     required this.mealEntity,
   });
@@ -12,88 +30,124 @@ class MealDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(mealEntity.nameMeal),
-        centerTitle: true
-      ),
-      body: SingleChildScrollView(
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(24)),
-              child: SizedBox(
-                height: 250,
-                width: double.infinity,
-                child: Image.network(
-                  mealEntity.mealThumb ?? '',
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Container(
-                      color: Colors.grey[200],
-                      child: Center(child: CircularProgressIndicator()),
-                    );
-                  },
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      color: Colors.grey[200],
-                      child: Center(
-                        child: Icon(
-                          Icons.broken_image,
-                          size: 100,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-    
-            Padding(
-              padding: const EdgeInsets.all(16.0),
+            PinnedCard(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  PinnedCard(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Ingredients',
-                          style: Theme.of(context).textTheme.titleLarge,
+                  Text(
+                    'Ingredients',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 8),
+                  ...(mealEntity.ingredients.isNotEmpty
+                      ? mealEntity.ingredients.map(
+                        (ingredient) => Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 2.0,
+                          ),
+                          child: Text('- $ingredient'),
                         ),
-                        const SizedBox(height: 8),
-                        ...mealEntity.ingredients.map(
-                          (ingredient) => Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 2.0),
-                            child: Text('- $ingredient'),
+                      )
+                      : [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 2.0,
+                          ),
+                          child: Text(
+                            'No ingredients available.',
+                            style: TextStyle(color: Colors.grey),
                           ),
                         ),
-                      ],
-                    ),
+                      ]),
+                ],
+              ),
+            ),
+            const SizedBox(height: 32),
+            PinnedCard(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Instructions',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  const SizedBox(height: 32), // Espacio mayor por la tachuela
-                  PinnedCard(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Instructions',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        const SizedBox(height: 8),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Text(mealEntity.mealInstructions ?? ''),
-                        ),
-                      ],
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Text(
+                      mealEntity.mealInstructions?.isNotEmpty == true
+                          ? mealEntity.mealInstructions!
+                          : 'No instructions available.',
+                      style: TextStyle(color: Colors.black87),
                     ),
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSilverAppBar extends StatelessWidget {
+  const CustomSilverAppBar({
+    super.key,
+    required this.mealEntity,
+  });
+
+  final MealEntity mealEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 250.0,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text(
+          mealEntity.nameMeal,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: Offset(0, 1),
+                blurRadius: 3,
+                color: Colors.black54,
+              ),
+            ],
+          ),
+        ),
+        centerTitle: true,
+        collapseMode: CollapseMode.parallax,
+        background: Image.network(
+          mealEntity.mealThumb ?? '',
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              color: Colors.grey[200],
+              child: Center(child: CircularProgressIndicator()),
+            );
+          },
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              color: Colors.grey[200],
+              child: Center(
+                child: Icon(
+                  Icons.broken_image,
+                  size: 100,
+                  color: Colors.grey,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
